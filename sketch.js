2f979ba24b8826dd7eef6a03ee7b768be97ad3e9
HK2Clutch <ghousia101@hotmail.com>
2020-11-26 23:07:46 -0500

    hanan

--- sketch.js
+++ sketch.js
@@ -9,9 +9,10 @@ var backgroundImg,platform;
 var bird, slingshot;
 
 var gameState = "onSling";
-
+var bg = "sprites/bg.png";
+var scorecount = 0;
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+    getbackgroundimg();
 }
 
 function setup(){
@@ -45,18 +46,26 @@ function setup(){
 }
 
 function draw(){
-    background(backgroundImg);
+    if(backgroundImg){
+        background(backgroundImg);
+    }
+    noStroke();
+    textSize(35);
+    fill("cyan")
+    text("score"+scorecount,600,30);
     Engine.update(engine);
     //strokeWeight(4);
     box1.display();
     box2.display();
     ground.display();
     pig1.display();
+    pig1.score();
     log1.display();
 
     box3.display();
     box4.display();
     pig3.display();
+    pig3.score();
     log3.display();
 
     box5.display();
@@ -85,4 +94,17 @@ function keyPressed(){
     if(keyCode === 32){
        // slingshot.attach(bird.body);
     }
+
+}
+async function getbackgroundimg(){
+    var meaningfull7 = await fetch("http://worldtimeapi.org/api/timezone/Asia/Tokyo")
+    var response = await meaningfull7.json();
+    var daytime =  response.datetime;
+    var hour = daytime.slice(11,13);
+    if(hour >= 06 &&hour <=19){
+    bg = "sprites/bg.png";
+
+    }
+    else{bg="sprites/bg2.jpg"}
+    backgroundImg = loadImage(bg);
 }
